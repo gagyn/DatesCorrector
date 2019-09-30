f2ebee891056d5595ba78942b8a93723e9a02240
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-09-30 21:38:40 +0200

    prepare for algorithm for getting date from file name

--- DatesCorrector/FileCorrector/DefaultCorrectStrategy.cs
+++ DatesCorrector/FileCorrector/DefaultCorrectStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using DatesCorrector.Models;
 
 namespace DatesCorrector.FileCorrector
@@ -7,7 +8,18 @@ namespace DatesCorrector.FileCorrector
     {
         public virtual void CorrectFile(ImageFile imageFile)
         {
-            throw new NotImplementedException();
+            File.SetCreationTime(imageFile.Path, GetDateTimeFromFileName(imageFile));
+        }
+
+        protected virtual DateTime GetDateTimeFromFileName(ImageFile imageFile)
+        {
+            // IMG_20190912.jpg => 12.09.2019
+            // IMG_02122018.jpg => 02.12.2018
+            var date = new DateTime();
+
+            // TODO: algotihm for getting date from name
+
+            return date;
         }
     }
 }
