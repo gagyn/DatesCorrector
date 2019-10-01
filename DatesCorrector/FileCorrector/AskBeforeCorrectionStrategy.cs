9ff83fba2e8a80401df620f76f384341969806d2
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-10-01 16:16:51 +0200

    show new date from file name when ask-before option is selected

--- DatesCorrector/FileCorrector/AskBeforeCorrectionStrategy.cs
+++ DatesCorrector/FileCorrector/AskBeforeCorrectionStrategy.cs
@@ -15,15 +15,17 @@ namespace DatesCorrector.FileCorrector
         {
             while (true)
             {
-                Console.Write($"{imageFile.Path} - date: {imageFile.PossibleDate} - Correct the file? (y/n): ");
+                Console.Write($"{imageFile.Path} - date: {imageFile.PossibleDate} - Correct the file to date: {GetDateTimeFromFileName(imageFile)}? (y/n): ");
                 var answer = Console.ReadKey().KeyChar;
                 Console.WriteLine();
 
                 switch (answer)
                 {
                     case 'y':
+                    case 'Y':
                         return true;
                     case 'n':
+                    case 'N':
                         return false;
                     default:
                         continue;
