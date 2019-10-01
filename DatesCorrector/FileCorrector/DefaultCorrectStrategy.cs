5857ec517bcb20b004456c1a6d4b9e6749f3c148
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-10-01 17:09:55 +0200

    comments

--- DatesCorrector/FileCorrector/DefaultCorrectStrategy.cs
+++ DatesCorrector/FileCorrector/DefaultCorrectStrategy.cs
@@ -13,11 +13,13 @@ namespace DatesCorrector.FileCorrector
 
         protected virtual DateTime GetDateTimeFromFileName(ImageFile imageFile)
         {
-            // IMG_20190912.jpg => 12.09.2019
-            // IMG_02122018.jpg => 02.12.2018
+            // IMG_20190912_232320.jpg => 12.09.2019 23:23:20
+            // IMG_02122018_124311.jpg => 02.12.2018 12:43:11
             var date = new DateTime();
 
             // TODO: algotihm for getting date from name
+            // search for year (4 chars) => 1990 - yearNow [2001 - 2012 might be months]
+            // get month and day on the right or left of year
 
             return date;
         }
